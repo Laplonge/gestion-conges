96900d96097b6b64b012c24b97d00993a15a721a
IN-ST-011 <IN-ST-011@IN-ST-011.home>
2019-04-17 14:46:17 +0200

    test modification

--- gestion-conges/src/main/java/fr/formation/inti/controller/InfosServlet.java
+++ gestion-conges/src/main/java/fr/formation/inti/controller/InfosServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class InfosServlet
+ * test
  */
 public class InfosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
