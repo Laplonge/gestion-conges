dc2fa5d2bd632d8148ee9401e692bf45f7f833fa
IN-ST-010 <IN-ST-010@IN-ST-010.home>
2019-04-17 11:19:43 +0200

    test avec code

--- gestion-conges/src/main/java/fr/formation/inti/controller/TestConflit.java
+++ gestion-conges/src/main/java/fr/formation/inti/controller/TestConflit.java
@@ -1,5 +1,95 @@
 package fr.formation.inti.controller;
 
-public class TestConflit {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import fr.formation.inti.entities.Conge;
+import fr.formation.inti.entities.Employe;
+import fr.formation.inti.service.interfaces.ICongeService;
+import fr.formation.inti.service.interfaces.IEmployeService;
+
+
+
+
+
+public class TestConflit {
+	private static final Log log = LogFactory.getLog(TestConflit.class);
+
+	public static void main(String[] args) throws ParseException {
+
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		IEmployeService es = (IEmployeService) context.getBean("employeService");
+//	
+//	List<Employe> allEmps = es.getAll();
+//	
+//	for (Employe emp : allEmps) {
+//		log.info(emp);
+//	}
+
+		ICongeService cs = (ICongeService) context.getBean("congeService");
+		List<Conge> allCong = cs.getAll();
+		for (Conge Cong : allCong) {
+			Employe emp = es.findById(Cong.getIdDemandesTraitees());
+
+			log.info("Employe: " + emp.getNom() + " " + emp.getPrenom() + ", Date de Début: " + Cong.getDateDebut()
+					+ ", Date de fin: " + Cong.getDateFin() + ", Date de demande: " + Cong.getDateDemande());
+		}
+		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date date = dateformat.parse("2019-06-19");
+		java.sql.Date sqlStartDate = new java.sql.Date(date.getTime());
+
+		date = dateformat.parse("2019-06-28");
+		java.sql.Date sqlFinDate = new java.sql.Date(date.getTime());
+
+		Date current = new Date();
+
+		java.sql.Date sqlcurrent = new java.sql.Date(current.getTime());
+
+		log.info(sqlcurrent);
+
+		for (Conge cong : allCong) {
+
+			//check if resquest is old or not
+			if (!(cong.getDateDebut().before(sqlcurrent)) && cong.getStatutDeLaDemande().equals("en cours")) {
+
+				log.info(cong.getDateDebut() + " " + cong.getDateFin());
+				// check if employee holidays is okay with the database
+				if ((sqlStartDate.after(cong.getDateDebut()) && sqlStartDate.before(cong.getDateFin()))
+						|| (sqlFinDate.after(cong.getDateDebut()) && sqlFinDate.before(cong.getDateFin()))
+						|| (sqlStartDate.equals(cong.getDateDebut()) || sqlFinDate.equals(cong.getDateFin()))) {
+					log.info("Demande refusé");
+					return;
+				}
+				// check if database is okay with the employee holidays
+				if ((cong.getDateDebut().after(sqlStartDate) && cong.getDateDebut().before(sqlFinDate))
+						|| (cong.getDateFin().after(sqlStartDate) && cong.getDateFin().before(sqlFinDate))) {
+					log.info("Demande refusé_2");
+					return;
+				} else {
+					log.info("Demande acceptée");
+				}
+			}
+		}
+		// check si vacances disponible
+
+//		EmployeDao edao = new EmployeDao();
+//		for (Employe emp : edao.getAll()) {
+//			System.out.println(emp);
+//		}
+//		Integer id = 1;
+//		log.debug(edao.findById(id));
+//		Employe emp = new Employe();
+//		emp.setPrenom("Marc");
+//		emp.setNom("Heurindélébil");
+
+		System.exit(0);
+	}
 }
